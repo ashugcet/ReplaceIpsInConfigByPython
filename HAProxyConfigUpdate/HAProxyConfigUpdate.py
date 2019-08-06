65e78de320f8481bd9d7969047f5a412ca3318e5
Ashutosh Kumar Dwivedi <ADwivedi@cpaglobal.com>
2019-08-06 21:45:49 +0530

    adding logic to not replace ips each time

--- HAProxyConfigUpdate/HAProxyConfigUpdate.py
+++ HAProxyConfigUpdate/HAProxyConfigUpdate.py
@@ -2,6 +2,7 @@
 #!/usr/bin/python
 import boto3
 import sys
+import subprocess
 Key1= sys.argv[1] 
 Key2= sys.argv[2] 
 boto3.setup_default_session(profile_name='default')
@@ -21,18 +22,43 @@ def list_instances_by_tag_value(tagkey, tagvalue):
    ips.append(instance["PrivateIpAddress"])
  return ips
 mm_iplist=list_instances_by_tag_value(Key1,Key2)
+#print(mm_iplist)
+print("\nActual master Ips:", mm_iplist)
 
-#print("\nActual master Ips:", mm_iplist)
+master_ips = []
+with open ('ectd.config', 'rt') as etcdconfig:
+    for line in etcdconfig:
+        master_ip_index = line.lower().replace(' ','').find('serverk8s-master-')        
+        if master_ip_index != -1:
+            #print(line.lower().replace(' ',''))
+            #print(line.lower().replace(' ','').find('serverk8s-master-'))
+            master_ip_end_index = line.lower().replace(' ','').find(':') 
+            master_ips.append(line.lower().replace(' ','')[18:master_ip_end_index])
+print("\nMaster Ips in config:", master_ips)
+unmatchedips=[]
+matchedips=[]
+for ip in mm_iplist:
+ if master_ips.count(ip) >= 1 :    
+    matchedips.append(ip)    
+ else :
+     unmatchedips.append(ip)
+   
+print("\nMatched IPs:",matchedips)
+print("\nUnmatched IPs:",unmatchedips)
 
-with open("ectd.config","r+") as etcdconfig:
- new_etcdconfig = etcdconfig.readlines()
- etcdconfig.seek(0)
- for line in new_etcdconfig:
-  if "k8s-master-" not in line:
-    etcdconfig.write(line)
- etcdconfig.truncate()
- i=0
- for ip in mm_iplist:
-  etcdconfig.write("server k8s-master-"+str(i)+" "+ip+":6443 check fall 3 rise 2\n")
-  i+=1
+if len(unmatchedips) > 0:
+ with open("ectd.config","r+") as etcdconfig_to_be_changed:
+  new_etcdconfig = etcdconfig_to_be_changed.readlines()
+  etcdconfig_to_be_changed.seek(0)
+  for line in new_etcdconfig:
+   if "k8s-master-" not in line:
+    etcdconfig_to_be_changed.write(line)
+  etcdconfig_to_be_changed.truncate()
+  i=0
+  for ip in mm_iplist:
+   etcdconfig_to_be_changed.write("server k8s-master-"+str(i)+" "+ip+":6443 check fall 3 rise 2\n")
+   i+=1
+  #command = ['service', 'haproxy', 'reload']
+  #subprocess.call(command, shell=True)
+ 
 
